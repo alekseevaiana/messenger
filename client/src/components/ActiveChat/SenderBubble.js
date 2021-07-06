0b7ead702986d54450429a925266f177e1638457
alekseevayana <alekseevayana@bk.ru>
2021-07-06 16:58:18 -0700

    add photo-url of other user to a chat

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -6,36 +6,44 @@ const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexDirection: "column",
-    alignItems: "flex-end"
+    alignItems: "flex-end",
   },
   date: {
     fontSize: 11,
     color: "#BECCE2",
     fontWeight: "bold",
-    marginBottom: 5
+    marginBottom: 5,
   },
   text: {
     fontSize: 14,
     color: "#91A3C0",
     letterSpacing: -0.2,
     padding: 8,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
   bubble: {
     background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px"
-  }
+    borderRadius: "10px 10px 0 10px",
+  },
 }));
 
 const SenderBubble = (props) => {
   const classes = useStyles();
-  const { time, text } = props;
+  const { time, text, otherUser } = props;
   return (
     <Box className={classes.root}>
       <Typography className={classes.date}>{time}</Typography>
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
+      <div>
+        <img
+          src={otherUser.photoUrl}
+          alt="other's user photo"
+          width="15"
+          height="15"
+        />
+      </div>
     </Box>
   );
 };
