375f1d19ff3ed74976da00b8c8ecda58fb0cc93d
alekseevayana <alekseevayana@bk.ru>
2021-07-06 17:11:32 -0700

    style avatar in SenderBubble

--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { Avatar, Box, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -25,6 +25,13 @@ const useStyles = makeStyles(() => ({
     background: "#F4F6FA",
     borderRadius: "10px 10px 0 10px",
   },
+  logo: {
+    marginTop: "12px",
+  },
+  userPicture: {
+    width: "30px",
+    height: "30px",
+  },
 }));
 
 const SenderBubble = (props) => {
@@ -36,14 +43,13 @@ const SenderBubble = (props) => {
       <Box className={classes.bubble}>
         <Typography className={classes.text}>{text}</Typography>
       </Box>
-      <div>
-        <img
+      <Box className={classes.logo}>
+        <Avatar
           src={otherUser.photoUrl}
-          alt="other's user photo"
-          width="15"
-          height="15"
+          alt={otherUser.username}
+          className={classes.userPicture}
         />
-      </div>
+      </Box>
     </Box>
   );
 };
