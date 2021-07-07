5b84868a2d4d87de5e6789f4ce6074aafcdbb49c
alekseevayana <alekseevayana@bk.ru>
2021-07-07 14:28:13 -0700

    sort conversations

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -8,16 +8,30 @@ const useStyles = makeStyles(() => ({
   root: {
     paddingLeft: 21,
     paddingRight: 21,
-    flexGrow: 1
+    flexGrow: 1,
   },
   title: {
     fontSize: 20,
     letterSpacing: -0.29,
     fontWeight: "bold",
     marginTop: 32,
-    marginBottom: 15
-  }
+    marginBottom: 15,
+  },
 }));
+
+function getLast(arr) {
+  return arr[arr.length - 1];
+}
+
+function compareConversations(conv1, conv2) {
+  const message1 = getLast(conv1.messages);
+  const message2 = getLast(conv2.messages);
+
+  const d1 = new Date(message1.createdAt);
+  const d2 = new Date(message2.createdAt);
+
+  return d2 - d1;
+}
 
 const Sidebar = (props) => {
   const classes = useStyles();
@@ -30,9 +44,17 @@ const Sidebar = (props) => {
       <Typography className={classes.title}>Chats</Typography>
       <Search handleChange={handleChange} />
       {conversations
-        .filter((conversation) => conversation.otherUser.username.includes(searchTerm))
+        .filter((conversation) =>
+          conversation.otherUser.username.includes(searchTerm)
+        )
+        .sort(compareConversations)
         .map((conversation) => {
-          return <Chat conversation={conversation} key={conversation.otherUser.username} />;
+          return (
+            <Chat
+              conversation={conversation}
+              key={conversation.otherUser.username}
+            />
+          );
         })}
     </Box>
   );
@@ -40,7 +62,7 @@ const Sidebar = (props) => {
 
 const mapStateToProps = (state) => {
   return {
-    conversations: state.conversations
+    conversations: state.conversations,
   };
 };
 
